084de986312862009ae108f5e69a0a0be4826832
Maksym Barvinskyi <Maksym_Barvinskyi@epam.com>
2019-10-08 17:04:15 +0300

    First commit

---
+++ src/main/java/org/coteji/core/Test.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2019 Coteji AUTHORS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.coteji.core;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Test {
+    private String name;
+    private List<String> steps;
+    private Map<String, Object> attributes;
+    private Set<String> labels;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public Set<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(Set<String> labels) {
+        this.labels = labels;
+    }
+}
